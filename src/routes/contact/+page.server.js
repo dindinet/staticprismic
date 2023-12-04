a31d6e8cde03076502e03845dc0640d4528cc241
dklongley <dklongley@gmail.com>
2023-12-04 19:13:05 +0000

    all the pages are go

---
+++ src/routes/contact/+page.server.js
@@ -0,0 +1,18 @@
+export async function load(){
+    const response = await fetch(
+        'https://codegen.plasmic.app/api/v1/loader/html/preview/tTqxxSNNPLamhhbvK9V5UT/ContactUs?hydrate=1&embedHydrate=1',
+        {
+          headers: {
+            // Your project ID and public API token
+            'x-plasmic-api-project-tokens': 'tTqxxSNNPLamhhbvK9V5UT:BFywrK12CIOe9GdI57EfvyxjW7xEs71PPMsXuClOsEgDAxUttSe822OGURTYgOQOTXiI4HudjQYhPEFyy46Q'
+            // You can find your PROJECT token by visiting
+            // https://studio.plasmic.app/projects/[tTqxxSNNPLamhhbvK9V5UT]/docs/loader#showToken=true
+          }
+        }
+      );
+      const result = await response.json();
+    return{
+            plasmic:result.html
+    };
+}
+
