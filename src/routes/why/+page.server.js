0a3e1b1cff667439c7b889a3ba582ccaef355d3c
dklongley <dklongley@gmail.com>
2023-12-05 17:56:42 +0000

    whypage in html in why route

--- src/routes/why/+page.server.js
+++ src/routes/why/+page.server.js
@@ -2,12 +2,12 @@ export async function load(){
     const response = await fetch(
         'https://primo-startup-vercel.vercel.app/why',
         {
-          headers: {
+          //headers: {
             // Your project ID and public API token
-            'x-plasmic-api-project-tokens': 'tTqxxSNNPLamhhbvK9V5UT:BFywrK12CIOe9GdI57EfvyxjW7xEs71PPMsXuClOsEgDAxUttSe822OGURTYgOQOTXiI4HudjQYhPEFyy46Q'
+            //'x-plasmic-api-project-tokens': 'tTqxxSNNPLamhhbvK9V5UT:BFywrK12CIOe9GdI57EfvyxjW7xEs71PPMsXuClOsEgDAxUttSe822OGURTYgOQOTXiI4HudjQYhPEFyy46Q'
             // You can find your PROJECT token by visiting
             // https://studio.plasmic.app/projects/[tTqxxSNNPLamhhbvK9V5UT]/docs/loader#showToken=true
-          }
+          //}
         }
       );
       const result = await response.text();
