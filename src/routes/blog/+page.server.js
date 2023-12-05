322ed040814fd9bf5f9ca43acb4058fb905d447d
dklongley <dklongley@gmail.com>
2023-12-05 19:02:18 +0000

    papo page in the blog

--- src/routes/blog/+page.server.js
+++ src/routes/blog/+page.server.js
@@ -12,6 +12,20 @@ export async function load(){
         }
       );
       const result = await response.json();
+      
+      const papohtml = await fetch(
+        'https://primo-startup-vercel.vercel.app/papo',
+        {
+          headers: {
+            // Your project ID and public API token
+            'x-plasmic-api-project-tokens': 'tTqxxSNNPLamhhbvK9V5UT:BFywrK12CIOe9GdI57EfvyxjW7xEs71PPMsXuClOsEgDAxUttSe822OGURTYgOQOTXiI4HudjQYhPEFyy46Q'
+            // You can find your PROJECT token by visiting
+            // https://studio.plasmic.app/projects/[tTqxxSNNPLamhhbvK9V5UT]/docs/loader#showToken=true
+          }
+        }
+      );
+      const paporesult = await papohtml.text();
+      
       const respfooter = await fetch(
         'https://codegen.plasmic.app/api/v1/loader/html/preview/tTqxxSNNPLamhhbvK9V5UT/Footer?hydrate=1&embedHydrate=1',
         {
@@ -26,7 +40,7 @@ export async function load(){
       const footerresult = await respfooter.json();
 
     return{
-            plasmic:result.html,plasfooter:footerresult.html
+            plasmic:result.html,papopage:paporesult,plasfooter:footerresult.html
     };
 }
 
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,4 +4,5 @@
 
 <div>{@html data.plasmic}</div>
 <section><center><h2>Bloggety Blog</h2></center></section>
+<section><div>{@html data.papopage}</div></section>
 <section><div>{@html data.plasfooter}</div></section>
