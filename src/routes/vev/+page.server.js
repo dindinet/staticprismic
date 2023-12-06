f251998bcacfaccb6328da0bee3689b57733db0f
dklongley <dklongley@gmail.com>
2023-12-06 17:42:34 +0000

    vev time

---
+++ src/routes/vev/+page.server.js
@@ -0,0 +1,11 @@
+export async function load(){
+    const response = await fetch(
+        'https://a-dindinet.vev.site/dindinet/',
+        {}
+      );
+      const result = await response.text();
+    return{
+            vevpage:result
+    };
+}
+
---
+++ src/routes/vev/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let data;
+</script>
+
+{@html data.vevpage}
+<style>#vevwatermark{display:none;}</style>
