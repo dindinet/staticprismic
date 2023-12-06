3614d0bac44e7b47e97971f393d6e4184d77f3fc
dklongley <dklongley@gmail.com>
2023-12-06 10:07:34 +0000

    wix page please

---
+++ src/routes/wix/+page.server.js
@@ -0,0 +1,19 @@
+export async function load(){
+    const response = await fetch(
+        'https://dklongley.wixstudio.io/papo',
+        {
+          //headers: {
+            // Your project ID and public API token
+            //'x-plasmic-api-project-tokens': 'tTqxxSNNPLamhhbvK9V5UT:BFywrK12CIOe9GdI57EfvyxjW7xEs71PPMsXuClOsEgDAxUttSe822OGURTYgOQOTXiI4HudjQYhPEFyy46Q'
+            // You can find your PROJECT token by visiting
+            // https://studio.plasmic.app/projects/[tTqxxSNNPLamhhbvK9V5UT]/docs/loader#showToken=true
+          //}
+        }
+      );
+      const result = await response.text();
+    return{
+            wixpage:result
+    };
+}
+//https://dklongley.wixstudio.io/papo
+
