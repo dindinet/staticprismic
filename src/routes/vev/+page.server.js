815b097f40d9618d3578bd594b4932e34794ba2a
dklongley <dklongley@gmail.com>
2023-12-06 18:33:03 +0000

    sick vev site

--- src/routes/vev/+page.server.js
+++ src/routes/vev/+page.server.js
@@ -2,7 +2,7 @@ export const prerender = true;
 
 export async function load(){
     const response = await fetch(
-        'https://a-dindinet.vev.site/review/',
+        'https://a-dindinet.vev.site/sick/',
         {}
       );
       const vevresult = await response.text();
