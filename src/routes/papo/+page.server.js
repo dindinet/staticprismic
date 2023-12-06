b175950c3e08ca190ad0cda087d0f3d285ccf586
dklongley <dklongley@gmail.com>
2023-12-06 16:02:34 +0000

    /build/

--- src/routes/papo/+page.server.js
+++ src/routes/papo/+page.server.js
@@ -13,7 +13,7 @@ export async function load(){
       const paporesult = await response.text();
 
     return{
-            papopage:paporesult.replaceAll('/build/_assets', 'https://saas-wh3p8.wstd.io/build/_assets').replaceAll('/cgi/image/', 'https://saas-wh3p8.wstd.io/cgi/image/')
+            papopage:paporesult.replaceAll('/build/_assets', 'https://saas-wh3p8.wstd.io/build/_assets').replaceAll('/cgi/image/', 'https://saas-wh3p8.wstd.io/cgi/image/').replaceAll('/build/','https://saas-wh3p8.wstd.io/build/')
     };
 }
 
