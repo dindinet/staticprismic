440e4895957b96f07d00c5234fb68ce8bdd141ea
dklongley <dklongley@gmail.com>
2023-12-08 14:53:56 +0000

    bali 60383

--- src/routes/vev/+page.server.js
+++ src/routes/vev/+page.server.js
@@ -2,10 +2,10 @@ export const prerender = true;
 
 export async function load(){
     const response = await fetch(
-        'https://a-dindinet.vev.site/review',
+        //'https://a-dindinet.vev.site/review',
         //'https://a-dindinet.vev.site/dindinet',
         //'https://a-dindinet.vev.site/sick?trick',
-        //'https://a-dindinet.vev.site/bali_60383',
+        'https://a-dindinet.vev.site/bali_60383',
         //'https://maxgoldhouse.com',
         //'https://wiltoninternational.com/',
         //'https://websitesforgrowth.com/',
