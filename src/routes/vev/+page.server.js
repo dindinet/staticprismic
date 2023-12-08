45f8b8d29190635891733fbb814b6d487cb252e9
dklongley <dklongley@gmail.com>
2023-12-08 17:58:16 +0000

    Clarity 2023

--- src/routes/vev/+page.server.js
+++ src/routes/vev/+page.server.js
@@ -2,16 +2,17 @@ export const prerender = true;
 
 export async function load(){
     const response = await fetch(
-        //'https://a-dindinet.vev.site/review',
+        'https://a-dindinet.vev.site/review',
         //'https://a-dindinet.vev.site/dindinet',
         //'https://a-dindinet.vev.site/sick?trick',
-        'https://a-dindinet.vev.site/bali_60383',
+        //'https://a-dindinet.vev.site/bali_60383',
         //'https://maxgoldhouse.com',
         //'https://wiltoninternational.com/',
         //'https://websitesforgrowth.com/',
         {}
       );
-    const vevresult = await response.text();
+    
+      const vevresult = await response.text();
       
     return{
       vevpage:vevresult
