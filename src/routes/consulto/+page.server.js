ebd5afcf4b59d8d2a2dd44c783418cf6637bd0bc
dklongley <dklongley@gmail.com>
2023-12-10 12:24:22 +0000

    consulto

---
+++ src/routes/consulto/+page.server.js
@@ -0,0 +1,18 @@
+export async function load(){
+    const response = await fetch(
+        'https://consulto.dcms.site/',
+        {
+          //headers: {
+            // Your project ID and public API token
+            //'x-plasmic-api-project-tokens': 'tTqxxSNNPLamhhbvK9V5UT:BFywrK12CIOe9GdI57EfvyxjW7xEs71PPMsXuClOsEgDAxUttSe822OGURTYgOQOTXiI4HudjQYhPEFyy46Q'
+            // You can find your PROJECT token by visiting
+            // https://studio.plasmic.app/projects/[tTqxxSNNPLamhhbvK9V5UT]/docs/loader#showToken=true
+          //}
+        }
+      );
+      const result = await response.text();
+    return{
+            consultopage:result//.replaceAll('/assets/','https://www.optimadesign.co.uk/assets/').replaceAll('href="/','href="https://www.optimadesign.co.uk/').replaceAll('/images/jcogs_img/','https://www.optimadesign.co.uk//images/jcogs_img/')
+    };
+}
+
